50c7135b3665be04e30eef006c3cd4044fb7ae5f
proler <proler@cf607b03-870e-abec-cebd-6e2755f62f97>
2011-10-07 10:55:18 +0000

    better perl lib

--- avrcmd.pm
+++ avrcmd.pm
@@ -18,31 +18,43 @@ to set com props you can start manually:
 mode COM1 BAUD=115200 PARITY=n DATA=8 STOP=1
 
 =cut
-
 #our %config;
 package avrcmd;
 use strict;
 use Time::HiRes qw(time sleep);
-#use Data::Dumper;
+use Data::Dumper;
+$Data::Dumper::Sortkeys = $Data::Dumper::Useqq = $Data::Dumper::Terse = 1;
+$Data::Dumper::Indent = 0;
 #our $port;
 #my @porttry;
 #my $portpath;
+sub debug (@) {
+  my $self = shift if ref $_[0];
+  return unless $self->{debug};
+  print join( ' ', @_ ), "\n";
+}
+
+sub check () {
+  my $self = shift if ref $_[0];
+  #$self->debug('chk run');
+  $self->open( $self->{wait} ), $self->init() unless $self->{port};
+  return 'no port' unless $self->{port};
+  my @status = $self->{port}->status();
+  #$self->debug( Dumper \@status );
+  $self->debug('no status, reopening'), $self->open( $self->{wait} ), $self->init() if @status <= 1;
+  return 'cant reopen port' unless $self->{port};
+  return;
+}
+
 sub read(;$) {
   my $self = shift if ref $_[0];
-  #Poll to see if any data is coming in
-  return unless $self->{port};
+  return if $self->check();
   my $end = time + $_[0];
   my $ret;
   do {
-    #print '.';
     while ( length( my $char = $self->{port}->lookfor() ) ) {
-      #If we get data, then print it
-      #if ( length $char ) {
-      #print "Recd [$char] \n";    #return $char;
       $ret .= $char;
-      #}
     }
-    #warn "[$ret]", ' ', time, ' ', $end if $ret;
     $self->parse($ret);
     sleep $self->{sleep} unless length $ret;
   } while !length $ret and time < $end;
@@ -51,18 +63,42 @@ sub read(;$) {
 
 sub say(;$) {
   my $self = shift if ref $_[0];
-  local $_ = $self->read(@_);
-  #local $_ = rd;
-  #print "read[", $_, "]" if length $_;
-  print $_, "\n" if length $_;
-  $_;
+  my $end = time + $_[0];
+  my $ret;
+  local $_;
+  do {
+    $ret .= ( $_ = $self->read(@_) );
+    print $_, "\n" if length $_;
+  } while time < $end;
+  return $ret;
 }
 
 sub write (@) {
   my $self = shift if ref $_[0];
-  return unless $self->{port};
+  return if $self->check();
   $self->{port}->write( join '', @_ );
-  #print "writing [", ( join '', @_ ), "]\n";
+}
+
+sub open (;$) {
+  my $self = shift if ref $_[0];
+  delete $self->{port};
+  sleep $_[0] if $_[0];
+  if ( $^O =~ /^(ms)?win/i ) {
+    eval q{use Win32::SerialPort;};
+    $self->{port} = Win32::SerialPort->new( $self->{path} ) unless $@;
+    warn "not opened [$self->{path}] [$@]" unless $self->{port};
+  }
+  unless ( $self->{port} ) {
+    eval q{use Device::SerialPort;};
+    $self->{port} = Device::SerialPort->new( $self->{path} ) unless $@;
+    warn "not opened [$self->{path}] [$@]" unless $self->{port};
+  }
+  return unless $self->{port};
+  $self->{port}->baudrate( $self->{'baudrate'} //= 115200 );
+  $self->{port}->databits( $self->{'databits'} //= 8 );
+  $self->{port}->parity( $self->{'parity'}     //= 'none' );
+  $self->{port}->stopbits( $self->{'stopbits'} //= 1 );
+  return $self->{port};
 }
 
 sub new {
@@ -72,12 +108,11 @@ sub new {
   else                             { bless( $self, $class ) unless ref $class; }
   $self->{split}   //= qr/\n/;
   $self->{handler} //= {
-    qr{^R} => sub { warn("readed[$_[0] by $_[1]]") }
+    #qr{^R} => sub { warn("readed[$_[0] by $_[1]]") }
   };
   $self->{path}  //= $self->{com};
   $self->{wait}  //= 2;
   $self->{sleep} //= 0.01;
-
   unless ( $self->{path} ) {
     if ( $^O =~ /^(ms|cyg)?win/i ) {
       $self->{try_from} //= 0;
@@ -94,35 +129,24 @@ sub new {
     }
   }
   for ( $self->{path} ? $self->{path} : @{ $self->{porttry} || [] } ) {
-    print "try [$_]\n" if $self->{debug};
+    $self->debug("try [$_]");
     $self->{path} = $_ if -e;
     next unless $self->{path};
-    print "selected port [$self->{path}] [$^O]\n" if $self->{debug};
-    #warn $^O;
-    if ( $^O =~ /^(ms)?win/i ) {
-      eval q{use Win32::SerialPort;};
-      $self->{port} = Win32::SerialPort->new( $self->{path} ) unless $@;
-      warn "not opened [$self->{path}] [$@]" unless $self->{port};
-    }
-    #$quiet
-    #|| die "Can't open $self->{port}Name: $^E\n";    # $quiet is optional
-    unless ( $self->{port} ) {
-      eval q{use Device::SerialPort;};
-      #my $self->{port} = Device::SerialPort->new("/dev/tty.usbserial");
-      #my $self->{port} = Device::SerialPort->new("COM2");
-      $self->{port} = Device::SerialPort->new( $self->{path} ) unless $@;
-      warn "not opened [$self->{path}] [$@]" unless $self->{port};
-    }
-    #die " Can't open port"
-    #warn Dumper $self->{port};
+    $self->debug("selected port [$self->{path}] [$^O]");
+    $self->open();
     last if $self->{port};
   }
   return unless $self->{port};
-  $self->{port}->baudrate( $self->{'baudrate'} //= 115200 );
-  $self->{port}->databits( $self->{'databits'} //= 8 );
-  $self->{port}->parity( $self->{'parity'}     //= 'none' );
-  $self->{port}->stopbits( $self->{'stopbits'} //= 1 );
-  if ( $^O =~ /^(ms|cyg)?win/i ) {
+  $self->debug( map { $self->{$_} . ' ' } qw(path baudrate databits parity stopbits) );
+  $self->init();
+  return $self;
+}
+
+sub init () {
+  my $self = shift if ref $_[0];
+  return unless $self->{port};
+  if ( $^O =~ /^(ms|cyg)?win/i and !$self->{no_mode} ) {
+    delete $self->{port};
     $self->{winmode} = "mode $self->{path} BAUD=$self->{'baudrate'} "
       . (
       !$self->{'parity'}
@@ -130,21 +154,18 @@ sub new {
       : "PARITY=" . ( $self->{'parity'} eq 'odd' ? 'o' : $self->{'parity'} eq 'even' ? 'e' : 'n' ) . " "
       ) . "DATA=$self->{'databits'} STOP=$self->{'stopbits'}";
     $_ = `$self->{winmode}`;
-    print $self->{winmode}, "\n", $_ if $self->{debug};
+    $self->debug( $self->{winmode}, "\n", $_ );
+    $self->open();
   }
-  print +( map { $self->{$_} . ' ' } qw(path baudrate databits parity stopbits) ), "\n" if $self->{debug};
-  #$self->{'waitinit'} //= 1;
+  return unless $self->{port};
   if ( $self->{'waitinit'} ) {
-    print "waiting init..\n" if $self->{debug};
+    $self->debug("waiting init..");
     my $n = 0;
     local $SIG{INT} = sub { $n = 99999; };
     local $| = 1;
-    print('i'),
-      #sleep 1,
-      $_ = $self->say(1) while $n++ < 10 and !/I/;
-    #warn "init[$_]";
+    print('i'), $_ = $self->say(1) while $n++ < 10 and !/I/;
   }
-  return $self;
+  $self->{init}->($self) if ref $self->{init} eq 'CODE';
 }
 
 sub cmd ($;@) {
@@ -170,15 +191,14 @@ sub pinMode ($$) {
 sub digitalRead ($) {
   my $self = shift if ref $_[0];
   my $ret;
-  #warn "dcmdgo";
+  warn "dcmdgo";
   local $self->{handler}{qr{r(?<pin>$_[0]),(?<state>\d+)}} = sub {
     #print "DR:pin $_[0] changed", Dumper \@_;
     $ret = $_[1]{state};
   };
   $self->cmd( 'r', @_ );
   my $tries = 3;
-  #warn ("dreadgo[$self->{wait}][$ret]:"),
-  $self->read( $self->{wait} ) while !length $ret and --$tries > 0;
+  warn("dreadgo[$self->{wait}][$ret]:"), $self->read( $self->{wait} ) while !length $ret and --$tries > 0;
   $ret;
 }
 
@@ -256,11 +276,7 @@ sub parse ($) {
 =todo
 pulseIn()
 =cut
-
 unless (caller) {
-  #parse("test");
-  #parse("R0");
-  #parse("R1\nR2\n");
   sub {
     local $| = 1;
     my $port = __PACKAGE__->new(
@@ -268,9 +284,7 @@ unless (caller) {
       debug => 1, waitinit => 1,
       #path=>'COM1',
     ) or return;
-    #sleep 1,
     $port->write( $_ . ' ' ), $port->say for @ARGV;
-    #sleep 10;
     my $t = time;
     local $SIG{INT} = sub { $t = 0; };
     $port->say while time - $t < 60;
--- examples/button.pl
+++ examples/button.pl
@@ -1,25 +1,30 @@
 #!/usr/bin/perl
 #
-# react on pressed button
+# react on pressed button[s]
 #
 use strict;
 use Data::Dumper;
 #use Time::HiRes;
 use lib::abs qw(..);
 use avrcmd;
-my $pin  = 3;
+#my $pin  = 3;
+my @pin  = ( 3 .. 10 );
 my $port = avrcmd->new(
   #'baudrate'=>9600,
   debug => 1, waitinit => 1,
   #path=>'COM1',
   handler => {
-    qr{r(?<pin>$pin),(?<state>\d+)} => sub { print "pin $pin changed", Dumper \@_ }
-  }
+    map {
+      qr{r(?<pin>$_),(?<state>\d+)} => sub { print "pin changed", Dumper( $_[1] ), "\n" }
+      } @pin
+  },
+  init => sub {
+    my $port = shift if ref $_[0];
+    $port->monitor( $_, 1 ) for @pin;
+    $port->say(2);    #wait for monitor answers
+  },
 ) or die;
-$port->monitor( $pin, 1 );
-#warn 'read:', $port->digitalRead( $pin );
+warn 'read:', $port->digitalRead(3);
 my $stop;
 local $SIG{INT} = sub { ++$stop };
-$port->say(0.1),
-  #Time::HiRes::sleep(0.1)
-  while !$stop;
+$port->say(0.1), while !$stop;
